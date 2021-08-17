f95bf2c20631bb5d2c22732e7dd483288a7e3eea
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-08-16 21:45:14 -0300

    feat: ensure SignUpController implements controller protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,12 @@
-export default class SignUpController {
-  handle(httpRequest: any): any {
+import { HttpResponse } from './../protocols/http';
+import { Controller } from '../protocols/controller';
+import { HttpRequest } from '../protocols/http';
+
+export default class SignUpController implements Controller {
+  handle(httpRequest: HttpRequest): HttpResponse {
     return {
       statusCode: 400,
+      body: null,
     };
   }
 }
