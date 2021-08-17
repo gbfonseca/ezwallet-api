65188a0c1727c63fe3ee4314e300613ef6c22f2a
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-08-17 11:58:09 -0300

    test: ensure SignUpController have is required field confirmPassword

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -61,4 +61,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('password'));
   });
+
+  test('should return 400 if no confirmPassword is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        lastName: 'any_lastName',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('confirmPassword'));
+  });
 });
