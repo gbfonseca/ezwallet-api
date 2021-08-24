3ae814495dd01356b39a6e1009e7c44f466a9a24
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-08-24 14:57:58 -0300

    test: ensure BcryptAdapter calls bcrypt with correct values

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,8 @@
 import { BcryptAdapter } from './bcrypt-adapter';
 // import bcrypt from 'bcrypt';
 
+const salt = 12;
+
 jest.mock('bcrypt', () => ({
   hash(data: string) {
     return 'hash_value';
@@ -9,9 +11,17 @@ jest.mock('bcrypt', () => ({
 
 describe('BcryptAdapter', () => {
   test('should return a hash if data is correct', async () => {
-    const sut = new BcryptAdapter();
+    const sut = new BcryptAdapter(salt);
     const password = 'any_password';
     const hash = await sut.encrypt(password);
     expect(hash).toBe('hash_value');
+  });
+
+  test('should BcryptAdapter calls bcrypt with correct values', async () => {
+    const sut = new BcryptAdapter(salt);
+    const hashSpy = jest.spyOn(sut, 'encrypt');
+    const password = 'any_password';
+    await sut.encrypt(password);
+    expect(hashSpy).toHaveBeenCalledWith('any_password');
   });
 });
