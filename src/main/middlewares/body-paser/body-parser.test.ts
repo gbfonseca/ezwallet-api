0a217897a525ddf22c6e7e4b33e19a3284cfee0e
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-08-31 16:09:49 -0300

    test: ensure Express Body Parser Middleware returns a json data

---
+++ src/main/middlewares/body-paser/body-parser.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import app from '../../config/app';
+
+describe('Body Parser Middleware', () => {
+  test('should app returns a json data', async () => {
+    app.post('/body_parser_test', (req, res) => {
+      res.send(req.body);
+    });
+
+    await request(app)
+      .post('/body_parser_test')
+      .send({ ok: 'ok' })
+      .expect({ ok: 'ok' });
+  });
+});
