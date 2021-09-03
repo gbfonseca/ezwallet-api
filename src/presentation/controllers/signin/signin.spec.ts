4406755213ff10c9553a015db66f57a19d921f34
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-09-03 11:26:16 -0300

    test: ensure SignInController returns 400 if not password is provided

---
+++ src/presentation/controllers/signin/signin.spec.ts
@@ -0,0 +1,23 @@
+import { MissingParamError } from './../../errors/missing-param-error';
+import { SignInController } from './signin';
+const makeSut = (): SignInController => {
+  const sut = new SignInController();
+
+  return sut;
+};
+
+describe('SignIn Controller', () => {
+  test('should returns 400 if not password is provided', async () => {
+    const sut = makeSut();
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('password'));
+  });
+});
