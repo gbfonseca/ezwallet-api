22443f2cad2b5ced925feda77271aa31abcbd347
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-09-04 17:45:34 -0300

    test: ensure DbAuthenticationAdapter throws if findUserByEmailRepository throws

--- src/data/usecases/authentication/db-authentication-adapter.spec.ts
+++ src/data/usecases/authentication/db-authentication-adapter.spec.ts
@@ -1,13 +1,42 @@
+import { FindUserByEmailRepository } from '../../protocols/find-user-by-email-repository';
+import { UserModel } from '../user/db-add-user-adapter-protocols';
 import { DbAuthenticationAdapter } from './db-authentication-adapter';
-const makeSut = (): DbAuthenticationAdapter => {
-  const sut = new DbAuthenticationAdapter();
 
-  return sut;
+interface SutTypes {
+  sut: DbAuthenticationAdapter;
+  findUserByEmailRepositoryStub: FindUserByEmailRepository;
+}
+
+const makeFindUserByEmailRepository = (): FindUserByEmailRepository => {
+  class FindUserByEmailRepositoryStub implements FindUserByEmailRepository {
+    async find(email: string): Promise<UserModel> {
+      const fakeUser = {
+        id: 'any_id',
+        name: 'any_name',
+        lastName: 'any_lastName',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      };
+
+      return new Promise((resolve) => resolve(fakeUser));
+    }
+  }
+
+  const findUserByEmailRepositoryStub = new FindUserByEmailRepositoryStub();
+
+  return findUserByEmailRepositoryStub;
+};
+
+const makeSut = (): SutTypes => {
+  const findUserByEmailRepositoryStub = makeFindUserByEmailRepository();
+  const sut = new DbAuthenticationAdapter(findUserByEmailRepositoryStub);
+
+  return { sut, findUserByEmailRepositoryStub };
 };
 
 describe('DbAuthentication Adapter', () => {
   test('should checkCredentials calls with correct values', async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const dbAuthenticationAdapterSpy = jest.spyOn(sut, 'checkCredentials');
     const httpRequest = {
       body: {
@@ -22,5 +51,22 @@ describe('DbAuthentication Adapter', () => {
       email: 'any_email@mail.com',
       password: 'any_password',
     });
+  });
+
+  test('should throws if findUserByEmailRepository throws', async () => {
+    const { sut, findUserByEmailRepositoryStub } = makeSut();
+    jest
+      .spyOn(findUserByEmailRepositoryStub, 'find')
+      .mockReturnValue(new Promise((resolve, reject) => reject(new Error())));
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.checkCredentials(httpRequest.body);
+
+    await expect(httpResponse).rejects.toThrow();
   });
 });
