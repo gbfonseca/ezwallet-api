b17a4dbbec050a07927c367acd1681f908abc1c4
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-09-05 21:51:59 -0300

    test: ensure BcrypterAdapter return true if password matched

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -7,18 +7,26 @@ jest.mock('bcrypt', () => ({
   hash(data: string) {
     return 'hash_value';
   },
+
+  compare(value: string, valueHashed: string) {
+    return true;
+  },
 }));
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt);
+};
 
 describe('BcryptAdapter', () => {
   test('should return a hash if data is correct', async () => {
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
     const password = 'any_password';
     const hash = await sut.encrypt(password);
     expect(hash).toBe('hash_value');
   });
 
   test('should BcryptAdapter calls bcrypt with correct values', async () => {
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
     const hashSpy = jest.spyOn(sut, 'encrypt');
     const password = 'any_password';
     await sut.encrypt(password);
@@ -26,7 +34,7 @@ describe('BcryptAdapter', () => {
   });
 
   test('should BcryptAdapter throws if bcrypt throw', async () => {
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
     jest
       .spyOn(bcrypt, 'hash')
       .mockImplementationOnce(
@@ -35,5 +43,16 @@ describe('BcryptAdapter', () => {
     const password = 'any_password';
     const promise = sut.encrypt(password);
     await expect(promise).rejects.toThrow();
+  });
+
+  test('should BcryptAdapter returns true if password matched', async () => {
+    const sut = makeSut();
+
+    const password = 'any_password';
+    const hashedPassword = 'hashed_password';
+
+    const matchedPassword = await sut.decrypt(password, hashedPassword);
+
+    expect(matchedPassword).toBe(true);
   });
 });
