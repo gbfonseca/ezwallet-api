516351d9e7782c489b51c08fe6734680a0ce6e59
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-09-06 13:00:48 -0300

    test: ensure SignIn Route returns 200 on success

---
+++ src/main/routes/signin-routes.test.ts
@@ -0,0 +1,47 @@
+import { TypeormHelper } from './../../infra/db/typeorm/helpers/typeorm-helper';
+import request from 'supertest';
+import app from '../config/app';
+describe('SignIn Route', () => {
+  beforeAll(async () => {
+    await TypeormHelper.connect({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: ['src/infra/db/typeorm/entities/**.ts'],
+      migrations: ['src/infra/db/typeorm/migrations/**.ts'],
+      migrationsRun: true,
+      cli: {
+        migrationsDir: './src/infra/db/typeorm/migrations',
+        entitiesDir: './src/infra/db/typeorm/entities',
+      },
+    });
+  });
+
+  afterAll(() => {
+    TypeormHelper.client.close();
+  });
+
+  beforeEach(async () => {
+    await TypeormHelper.clear();
+  });
+
+  test('should SignIn Route returns an user and token', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Gabriel',
+        lastName: 'Fonseca',
+        email: 'userx@mail.com',
+        password: '123123',
+        confirmPassword: '123123',
+      })
+      .expect(200);
+
+    await request(app)
+      .post('/api/signin')
+      .send({
+        email: 'userx@mail.com',
+        password: '123123',
+      })
+      .expect(200);
+  });
+});
