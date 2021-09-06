310db875f92d3ad3b75a295f10721c1073e8a179
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-09-05 21:51:11 -0300

    feat: ensure BcrypterAdapter return true if password matched

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,11 +1,16 @@
-import { Encrypter } from '../../data/protocols/encrypter';
 import bcrypt from 'bcrypt';
+import { Encrypter } from '../../data/protocols/encrypter';
+import { Decrypter } from '../../data/protocols/decrypter';
 
-export class BcryptAdapter implements Encrypter {
+export class BcryptAdapter implements Encrypter, Decrypter {
   constructor(private readonly salt: number) {}
 
   async encrypt(value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt);
     return hash;
+  }
+
+  async decrypt(password: string, passwordHash: string): Promise<boolean> {
+    return await bcrypt.compare(password, passwordHash);
   }
 }
