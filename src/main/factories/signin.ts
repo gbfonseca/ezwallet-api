b7b7b6cd06048c3e35aceaba104e368f407b1efe
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-09-06 13:00:02 -0300

    feat: ensure SignIn Route returns 200 on success

---
+++ src/main/factories/signin.ts
@@ -0,0 +1,24 @@
+import { BcryptAdapter } from './../../infra/cryptography/bcrypt-adapter';
+import { UserTypeormRepository } from './../../infra/db/typeorm/repositories/user/user';
+import { DbAuthenticationAdapter } from './../../data/usecases/authentication/db-authentication-adapter';
+import { EmailValidatorAdapter } from './../../utils/EmailValidatorAdapter/email-validator-adapter';
+import { SignInController } from './../../presentation/controllers/signin/signin';
+import { JwtAdapter } from '../../infra/jwt/jwt-adapter';
+export const makeSignInController = (): SignInController => {
+  const SALT = 12;
+  const emailValidator = new EmailValidatorAdapter();
+  const userTypeormRepository = new UserTypeormRepository();
+  const bcryptAdapter = new BcryptAdapter(SALT);
+  const jwtAdapter = new JwtAdapter();
+  const dbAuthentication = new DbAuthenticationAdapter(
+    userTypeormRepository,
+    bcryptAdapter,
+    jwtAdapter,
+  );
+  const signInController = new SignInController(
+    emailValidator,
+    dbAuthentication,
+  );
+
+  return signInController;
+};
---
+++ src/main/routes/signin-routes.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import { adaptRoute } from './../adapters/express-router-adapter';
+import { makeSignInController } from '../factories/signin';
+export default (router: Router): void => {
+  router.post('/signin', adaptRoute(makeSignInController()));
+};
