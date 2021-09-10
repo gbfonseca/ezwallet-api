e3bb54c6a4c6d83c0b8bda77a9ae293bfac0f5a6
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-09-10 15:49:38 -0300

    feat: ensure LoggedUser returns 500 if throws

--- src/presentation/controllers/auth/logged-user/logged-user.ts
+++ src/presentation/controllers/auth/logged-user/logged-user.ts
@@ -1,16 +1,20 @@
-import { badRequest } from './../../../helpers/http-helper';
+import { badRequest, serverError } from './../../../helpers/http-helper';
 import { HttpRequest, HttpResponse } from '../../../protocols/http';
 import { Controller } from './../../../protocols/controller';
 export class LoggedUser implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const {
-      headers: { Authorization },
-    } = httpRequest;
+    try {
+      const {
+        headers: { Authorization },
+      } = httpRequest;
 
-    if (!Authorization) {
-      return badRequest(new Error('Authorization must be provided'));
+      if (!Authorization) {
+        return badRequest(new Error('Authorization must be provided'));
+      }
+
+      return new Promise((resolve) => resolve(null));
+    } catch (error) {
+      return serverError();
     }
-
-    return new Promise((resolve) => resolve(null));
   }
 }
