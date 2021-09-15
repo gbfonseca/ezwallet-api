30389771b6ffbb13712cfce9b4d53ec183365c8b
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-09-15 10:53:55 -0300

    test: ensure AuthenticatedRoute Middleware returns 400 if invalid token provided

--- src/main/middlewares/authenticated-route.test.ts
+++ src/main/middlewares/authenticated-route.test.ts
@@ -1,8 +1,24 @@
 import request from 'supertest';
 import app from '../config/app';
 describe('Authenticated Route', () => {
-  test('should return an user on success', async () => {
+  test('should return 400 if invalid token provided', async () => {
     app.get('/authenticated_test', (req, res) => {
+      if (!req.headers.authorization) {
+        res
+          .status(400)
+          .json({ auth: false, message: 'Invalid token provided' });
+      }
+      res.send(req.body);
+    });
+    await request(app).get('/authenticated_test').expect(400);
+  });
+  test('should return 200 if token is valid', async () => {
+    app.get('/authenticated_test', (req, res) => {
+      if (!req.headers.authorization) {
+        res
+          .status(400)
+          .json({ auth: false, message: 'Invalid token provided' });
+      }
       res.send(req.body);
     });
     await request(app)
