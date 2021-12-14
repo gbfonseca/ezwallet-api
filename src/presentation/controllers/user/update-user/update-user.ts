3d989a30cd75f57f9cc34e20c766128d56ddafd5
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-12-14 12:34:10 -0300

    feat: ensure UpdateUserController return 400 if invalid user provided

--- src/presentation/controllers/user/update-user/update-user.ts
+++ src/presentation/controllers/user/update-user/update-user.ts
@@ -1,9 +1,16 @@
+import { badRequest } from './../../../helpers/http-helper';
 import { UserModel } from './../../../../domain/models/user';
 import { HttpRequest, HttpResponse } from '../../../protocols/http';
 import { Controller } from './../../../protocols/controller';
 
 export default class UpdateUserController implements Controller {
-  handle(httpRequest: HttpRequest): Promise<HttpResponse<UserModel>> {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse<UserModel>> {
+    const { user } = httpRequest;
+
+    if (!user) {
+      return badRequest(new Error('Usuário inválido.'));
+    }
+
     return new Promise((resolve) => resolve(null));
   }
 }
