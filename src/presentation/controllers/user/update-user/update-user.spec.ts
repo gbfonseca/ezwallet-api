3957924224da2ee161c181ca82118c9138954f82
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-12-14 12:17:39 -0300

    test: ensure UpdateUserController calls with correct values

---
+++ src/presentation/controllers/user/update-user/update-user.spec.ts
@@ -0,0 +1,36 @@
+import UpdateUserController from './update-user';
+
+interface SutTypes {
+  sut: UpdateUserController;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new UpdateUserController();
+
+  return { sut };
+};
+
+describe('Update User Controller', () => {
+  it('should calls UpdateUserController with correct values', async () => {
+    const { sut } = makeSut();
+    const sutSpy = jest.spyOn(sut, 'handle');
+    const httpRequest = {
+      user: {
+        id: 'any_id',
+        name: 'any_name',
+        lastName: 'any_lastName',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      },
+      body: {
+        name: 'new_name',
+        lastName: 'any_lastName',
+        email: 'any_email@mail.com',
+      },
+    };
+
+    await sut.handle(httpRequest);
+
+    expect(sutSpy).toHaveBeenCalledWith(httpRequest);
+  });
+});
