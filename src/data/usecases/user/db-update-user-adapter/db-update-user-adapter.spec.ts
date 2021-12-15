0ef4ff40c2d771c83f5d82ba1a3999589f3d95a0
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-12-15 20:24:07 -0300

    test: ensure DbUpdateUserAdapter throws if UpdateUserRepository throws

--- src/data/usecases/user/db-update-user-adapter/db-update-user-adapter.spec.ts
+++ src/data/usecases/user/db-update-user-adapter/db-update-user-adapter.spec.ts
@@ -1,12 +1,35 @@
+import { UpdateUserModel } from '../../../../domain/usecases/update-user';
+import { UserModel } from '../db-add-user-adapter/db-add-user-adapter-protocols';
+import { UpdateUserRepository } from './../../../protocols/update-user-repository';
 import { DbUpdateUserAdapter } from './db-update-user-adapter';
 interface SutTypes {
   sut: DbUpdateUserAdapter;
+  updateUserRepositoryStub: UpdateUserRepository;
 }
 
-const makeSut = (): SutTypes => {
-  const sut = new DbUpdateUserAdapter();
+const makeUpdateUserRepository = (): UpdateUserRepository => {
+  class UpdateUserRepositoryStub implements UpdateUserRepository {
+    async update(id: string, updateUser: UpdateUserModel): Promise<UserModel> {
+      const fakeUpdatedUser = {
+        id: 'any_id',
+        name: 'new_name',
+        lastName: 'any_lastName',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      };
 
-  return { sut };
+      return new Promise((resolve) => resolve(fakeUpdatedUser));
+    }
+  }
+
+  return new UpdateUserRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const updateUserRepositoryStub = makeUpdateUserRepository();
+  const sut = new DbUpdateUserAdapter(updateUserRepositoryStub);
+
+  return { sut, updateUserRepositoryStub };
 };
 
 describe('DbUpdateUserAdapter', () => {
@@ -26,5 +49,27 @@ describe('DbUpdateUserAdapter', () => {
     await sut.update(request.id, request.data);
 
     expect(sutSpy).toHaveBeenCalledWith(request.id, request.data);
+  });
+
+  it('should throws if UpdateUserRepository throws ', async () => {
+    const { sut, updateUserRepositoryStub } = makeSut();
+    jest
+      .spyOn(updateUserRepositoryStub, 'update')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const request = {
+      id: 'any_id',
+      data: {
+        name: 'new_name',
+        lastName: 'any_lastName',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      },
+    };
+
+    const promise = sut.update(request.id, request.data);
+
+    await expect(promise).rejects.toThrow();
   });
 });
