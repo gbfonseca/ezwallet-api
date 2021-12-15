03740a88e07cc3e22468aed74ff847e53cb645f5
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-12-15 20:27:46 -0300

    feat: ensure DbUpdateUserAdapter return an user on success

--- src/data/usecases/user/db-update-user-adapter/db-update-user-adapter.ts
+++ src/data/usecases/user/db-update-user-adapter/db-update-user-adapter.ts
@@ -9,7 +9,7 @@ export class DbUpdateUserAdapter implements UpdateUser {
   constructor(private readonly updateUserRepository: UpdateUserRepository) {}
 
   async update(id: string, data: UpdateUserModel): Promise<UserModel> {
-    await this.updateUserRepository.update(id, data);
-    return new Promise((resolve) => resolve(null));
+    const updatedUser = await this.updateUserRepository.update(id, data);
+    return updatedUser;
   }
 }
