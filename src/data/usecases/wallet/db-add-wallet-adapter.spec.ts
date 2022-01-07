7028468bc45fd825b6fb2e5ee2ce7bb97ea9f084
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-07 17:20:31 -0300

    test: ensure DbAddWalletAdapter throws if AddWalletRepository throws

--- src/data/usecases/wallet/db-add-wallet-adapter.spec.ts
+++ src/data/usecases/wallet/db-add-wallet-adapter.spec.ts
@@ -1,13 +1,43 @@
+import { UserModel } from '../../../domain/models/user';
+import { WalletModel } from '../../../domain/models/wallet';
+import { AddWalletModel } from '../../../domain/usecases/wallet/add-wallet';
+import { AddWalletRepository } from '../../protocols/add-wallet-repository';
 import { DbAddWalletAdapter } from './db-add-wallet-adapter';
 
 interface SutTypes {
   sut: DbAddWalletAdapter;
+  addWalletRepositoryStub: AddWalletRepository;
 }
 
-const makeSut = (): SutTypes => {
-  const sut = new DbAddWalletAdapter();
+const makeAddWalletRepository = (): AddWalletRepository => {
+  class AddWalletRepositoryStub implements AddWalletRepository {
+    async add(
+      addWallet: AddWalletModel,
+      user: UserModel,
+    ): Promise<WalletModel> {
+      const fakeWallet: WalletModel = {
+        id: 'any_id',
+        name: 'Any Wallet Name',
+        user: {
+          id: 'any_id',
+          name: 'any_name',
+          lastName: 'any_lastName',
+          email: 'any_email@mail.com',
+          password: 'any_password',
+        },
+      };
+      return new Promise((resolve) => resolve(fakeWallet));
+    }
+  }
 
-  return { sut };
+  return new AddWalletRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const addWalletRepositoryStub = makeAddWalletRepository();
+  const sut = new DbAddWalletAdapter(addWalletRepositoryStub);
+
+  return { sut, addWalletRepositoryStub };
 };
 
 describe('DbAddWalletAdapter', () => {
@@ -23,11 +53,34 @@ describe('DbAddWalletAdapter', () => {
       name: 'any_name',
       lastName: 'any_lastName',
       email: 'any_email@mail.com',
-      password: 'any_password',
     };
 
     await sut.add(addWallet, user);
 
     expect(sutSpy).toBeCalledWith(addWallet, user);
+  });
+
+  it('should throws if AddWalletRepository throws', async () => {
+    const { sut, addWalletRepositoryStub } = makeSut();
+    jest
+      .spyOn(addWalletRepositoryStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+
+    const addWallet = {
+      name: 'Any Wallet Name',
+    };
+
+    const user = {
+      id: 'any_id',
+      name: 'any_name',
+      lastName: 'any_lastName',
+      email: 'any_email@mail.com',
+    };
+
+    const promise = sut.add(addWallet, user);
+
+    await expect(promise).rejects.toThrow();
   });
 });
