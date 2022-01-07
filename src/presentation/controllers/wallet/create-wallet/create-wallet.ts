8e7df3d01b02a081905f07f7f763e67177bfb32c
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-07 12:11:04 -0300

    feat: ensure CreateWalletController return 400 if no wallet provided

--- src/presentation/controllers/wallet/create-wallet/create-wallet.ts
+++ src/presentation/controllers/wallet/create-wallet/create-wallet.ts
@@ -1,7 +1,17 @@
+import { MissingParamError } from '../../../errors';
+import { badRequest } from '../../../helpers';
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols';
 
 export class CreateWalletController implements Controller {
   async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<any>> {
+    const requiredFields = ['name'];
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
+    }
+
     return new Promise((resolve) => resolve(null));
   }
 }
