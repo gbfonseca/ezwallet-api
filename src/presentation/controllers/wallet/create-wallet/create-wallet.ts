da1bd89fcbf19193b866d9ab8b13417103f4eb07
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-07 12:18:51 -0300

    feat: add HttpRequestBodyType

--- src/presentation/controllers/wallet/create-wallet/create-wallet.ts
+++ src/presentation/controllers/wallet/create-wallet/create-wallet.ts
@@ -2,8 +2,14 @@ import { MissingParamError } from '../../../errors';
 import { badRequest } from '../../../helpers';
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols';
 
+type HttpRequestBodyType = {
+  name: string;
+};
+
 export class CreateWalletController implements Controller {
-  async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<any>> {
+  async handle(
+    httpRequest: HttpRequest<HttpRequestBodyType>,
+  ): Promise<HttpResponse<any>> {
     const requiredFields = ['name'];
 
     for (const field of requiredFields) {
