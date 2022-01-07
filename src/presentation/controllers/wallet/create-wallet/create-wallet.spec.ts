040eb96b66fdc49433d291f774f363eecea49c20
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-07 14:31:13 -0300

    test: ensure CreateWalletController return 500 if AddWallet throws

--- src/presentation/controllers/wallet/create-wallet/create-wallet.spec.ts
+++ src/presentation/controllers/wallet/create-wallet/create-wallet.spec.ts
@@ -1,14 +1,45 @@
+import { WalletModel } from '../../../../domain/models/wallet';
+import {
+  AddWallet,
+  AddWalletModel,
+} from '../../../../domain/usecases/wallet/add-wallet';
 import { HttpRequest } from '../../../protocols';
+import { UserModel } from '../../auth/signup/signup-protocols';
 import { CreateWalletController } from './create-wallet';
 
 interface SutTypes {
   sut: CreateWalletController;
+  addWalletStub: AddWallet;
 }
 
-const makeSut = (): SutTypes => {
-  const sut = new CreateWalletController();
+const makeAddWallet = (): AddWallet => {
+  class AddWalletStub implements AddWalletStub {
+    async add(
+      addWallet: AddWalletModel,
+      user: UserModel,
+    ): Promise<WalletModel> {
+      const fakeWallet: WalletModel = {
+        id: 'any_id',
+        name: 'Any Wallet Name',
+        user: {
+          id: 'any_id',
+          name: 'any_name',
+          lastName: 'any_lastName',
+          email: 'any_email@mail.com',
+          password: 'any_password',
+        },
+      };
+      return new Promise((resolve) => resolve(fakeWallet));
+    }
+  }
+  return new AddWalletStub();
+};
 
-  return { sut };
+const makeSut = (): SutTypes => {
+  const addWalletStub = makeAddWallet();
+  const sut = new CreateWalletController(addWalletStub);
+
+  return { sut, addWalletStub };
 };
 
 describe('CreateWalletController', () => {
@@ -52,5 +83,30 @@ describe('CreateWalletController', () => {
     const httpResponse = await sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
+  });
+
+  it('should return 500 if AddWallet throws', async () => {
+    const { sut, addWalletStub } = makeSut();
+    jest
+      .spyOn(addWalletStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'Any Wallet Name',
+      },
+      user: {
+        id: 'any_id',
+        name: 'any_name',
+        lastName: 'any_lastName',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
