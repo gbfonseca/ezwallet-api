d7d2ca59641858f57260b39e5e8ea64e6ed42589
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-08 00:24:00 -0300

    feat: ensure DbFindWalletsByUserIdAdapter throws if FindWalletsByUserIdRepository throws

--- src/data/usecases/wallet/db-find-wallets-by-user-id-adapter/db-find-wallets-by-user-id-adapter.ts
+++ src/data/usecases/wallet/db-find-wallets-by-user-id-adapter/db-find-wallets-by-user-id-adapter.ts
@@ -1,8 +1,14 @@
 import { WalletModel } from '../../../../domain/models/wallet';
 import { FindWalletsByUserId } from '../../../../domain/usecases/wallet/find-wallets-by-user-id';
+import { FindWalletsByUserIdRepository } from '../../../protocols/find-wallets-by-user-id-repository';
 
 export class DbFindWalletsByUserIdAdapter implements FindWalletsByUserId {
+  constructor(
+    private readonly findWalletsByUserIdRepository: FindWalletsByUserIdRepository,
+  ) {}
+
   async find(id: string): Promise<WalletModel[]> {
+    await this.findWalletsByUserIdRepository.find(id);
     return new Promise((resolve) => resolve(null));
   }
 }
