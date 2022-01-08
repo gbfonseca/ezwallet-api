851d933c549575858dba2c7a3c9674ec755ea3ec
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-07 23:55:44 -0300

    feat: ensure FindWalletController return 400 if invalid user provided on request

--- src/presentation/controllers/wallet/find-wallet/find-wallet.ts
+++ src/presentation/controllers/wallet/find-wallet/find-wallet.ts
@@ -1,11 +1,18 @@
 import {
+  badRequest,
   Controller,
   HttpRequest,
   HttpResponse,
 } from '../create-wallet/create-wallet-protocols';
 
 export class FindWalletController implements Controller {
-  async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<any>> {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse<any>> {
+    const { user } = httpRequest;
+
+    if (!user) {
+      return badRequest(new Error());
+    }
+
     return new Promise((resolve) => resolve(null));
   }
 }
