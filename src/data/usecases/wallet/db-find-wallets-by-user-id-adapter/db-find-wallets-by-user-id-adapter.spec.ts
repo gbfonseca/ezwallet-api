e1468a3aef754b7d97dfa8ce3f892ccb514dc677
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-08 00:24:18 -0300

    test: ensure DbFindWalletsByUserIdAdapter throws if FindWalletsByUserIdRepository throws

--- src/data/usecases/wallet/db-find-wallets-by-user-id-adapter/db-find-wallets-by-user-id-adapter.spec.ts
+++ src/data/usecases/wallet/db-find-wallets-by-user-id-adapter/db-find-wallets-by-user-id-adapter.spec.ts
@@ -1,13 +1,41 @@
+import { WalletModel } from '../../../../domain/models/wallet';
 import { DbFindWalletsByUserIdAdapter } from './db-find-wallets-by-user-id-adapter';
-
+import { FindWalletsByUserIdRepository } from '../../../protocols/find-wallets-by-user-id-repository';
 interface SutTypes {
   sut: DbFindWalletsByUserIdAdapter;
+  findWalletsByUserIdRepositoryStub: FindWalletsByUserIdRepository;
 }
 
-const makeSut = (): SutTypes => {
-  const sut = new DbFindWalletsByUserIdAdapter();
+const makeFindWalletsByUserIdRepository = (): FindWalletsByUserIdRepository => {
+  class FindWalletsByUserIdRepositoryStub
+    implements FindWalletsByUserIdRepository
+  {
+    find(id: string): Promise<WalletModel[]> {
+      const fakeWallet = {
+        id: 'any_id',
+        name: 'any_name',
+        user: {
+          id: 'any_id',
+          email: 'any_email@mail.com',
+          name: 'any_name',
+          lastName: 'any_lastName',
+        },
+      };
 
-  return { sut };
+      return new Promise((resolve) => resolve([fakeWallet]));
+    }
+  }
+
+  return new FindWalletsByUserIdRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const findWalletsByUserIdRepositoryStub = makeFindWalletsByUserIdRepository();
+  const sut = new DbFindWalletsByUserIdAdapter(
+    findWalletsByUserIdRepositoryStub,
+  );
+
+  return { sut, findWalletsByUserIdRepositoryStub };
 };
 
 describe('DbFindWalletsByUserId Adapter', () => {
@@ -20,5 +48,19 @@ describe('DbFindWalletsByUserId Adapter', () => {
     await sut.find(id);
 
     expect(sutSpy).toHaveBeenCalledWith(id);
+  });
+  it('should throws if FindWalletsByUserIdRepository throws', async () => {
+    const { sut, findWalletsByUserIdRepositoryStub } = makeSut();
+    jest
+      .spyOn(findWalletsByUserIdRepositoryStub, 'find')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+
+    const id = 'any_id';
+
+    const promise = sut.find(id);
+
+    await expect(promise).rejects.toThrow();
   });
 });
