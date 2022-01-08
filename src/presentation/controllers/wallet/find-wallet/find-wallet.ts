1d1b5bb9a8edb8399df1e3045d25b20b87124155
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-08 00:05:23 -0300

    feat: ensure FindWalletController return 500 if FindWalletsByUserId throws

--- src/presentation/controllers/wallet/find-wallet/find-wallet.ts
+++ src/presentation/controllers/wallet/find-wallet/find-wallet.ts
@@ -1,18 +1,28 @@
+import { FindWalletsByUserId } from '../../../../domain/usecases/wallet/find-wallets-by-user-id';
 import {
   badRequest,
   Controller,
   HttpRequest,
   HttpResponse,
+  serverError,
 } from '../create-wallet/create-wallet-protocols';
 
 export class FindWalletController implements Controller {
+  constructor(private readonly findWalletsByUserId: FindWalletsByUserId) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse<any>> {
-    const { user } = httpRequest;
+    try {
+      const { user } = httpRequest;
 
-    if (!user) {
-      return badRequest(new Error());
+      if (!user) {
+        return badRequest(new Error());
+      }
+
+      await this.findWalletsByUserId.find(user.id);
+
+      return new Promise((resolve) => resolve(null));
+    } catch (error) {
+      return serverError();
     }
-
-    return new Promise((resolve) => resolve(null));
   }
 }
