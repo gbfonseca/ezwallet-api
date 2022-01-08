bb1ed70b7d3d509f762d3e51cdab32e210268f97
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-08 00:14:56 -0300

    test: ensure DbFindWalletsByUserIdAdapter calls with correct values

---
+++ src/data/usecases/wallet/db-find-walelts-by-user-id-adapter/db-find-walelts-by-user-id-adapter.spec.ts
@@ -0,0 +1,24 @@
+import { DbFindWalletsByUserIdAdapter } from './db-find-walelts-by-user-id-adapter';
+
+interface SutTypes {
+  sut: DbFindWalletsByUserIdAdapter;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new DbFindWalletsByUserIdAdapter();
+
+  return { sut };
+};
+
+describe('DbFindWalletsByUserId Adapter', () => {
+  it('should calls DbFindWalletsByUserIdAdapter with correct values', async () => {
+    const { sut } = makeSut();
+    const sutSpy = jest.spyOn(sut, 'find');
+
+    const id = 'any_id';
+
+    await sut.find(id);
+
+    expect(sutSpy).toHaveBeenCalledWith(id);
+  });
+});
