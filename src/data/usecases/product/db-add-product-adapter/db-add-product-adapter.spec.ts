8cdacdf36d425698ece6ee574b3c89db0dc6cadf
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-10 16:41:54 -0300

    test: ensure DbAddProductAdapter throws if AddProductRepository throws

--- src/data/usecases/product/db-add-product-adapter/db-add-product-adapter.spec.ts
+++ src/data/usecases/product/db-add-product-adapter/db-add-product-adapter.spec.ts
@@ -2,10 +2,16 @@ import { WalletModel } from '../../../../domain/models/wallet';
 import { DbAddProductAdapter } from './db-add-product-adapter';
 import { fakeWallet } from '../../../../../tests/factories/fake-wallet';
 import { FindWalletByIdRepository } from '../../../protocols/find-wallet-by-id-repository';
+import {
+  AddProductRepository,
+  AddProductRepositoryData,
+} from '../../../protocols/add-product-repository';
+import { ProductModel } from '../../../../domain/models/product';
 
 interface SutTypes {
   sut: DbAddProductAdapter;
   findWalletByIdRepositoryStub: FindWalletByIdRepository;
+  addProductRepositoryStub: AddProductRepository;
 }
 
 const makeFindWalletByIdRepository = (): FindWalletByIdRepository => {
@@ -23,11 +29,30 @@ const makeFindWalletByIdRepository = (): FindWalletByIdRepository => {
   return new FindWalletByIdRepositoryStub();
 };
 
+const makeAddProductRepository = (): AddProductRepository => {
+  class AddProductRepositoryStub implements AddProductRepository {
+    async add(addProduct: AddProductRepositoryData): Promise<ProductModel> {
+      return new Promise((resolve) =>
+        resolve({
+          id: 'any_id',
+          ...addProduct,
+        }),
+      );
+    }
+  }
+
+  return new AddProductRepositoryStub();
+};
+
 const makeSut = (): SutTypes => {
   const findWalletByIdRepositoryStub = makeFindWalletByIdRepository();
-  const sut = new DbAddProductAdapter(findWalletByIdRepositoryStub);
+  const addProductRepositoryStub = makeAddProductRepository();
+  const sut = new DbAddProductAdapter(
+    findWalletByIdRepositoryStub,
+    addProductRepositoryStub,
+  );
 
-  return { sut, findWalletByIdRepositoryStub };
+  return { sut, findWalletByIdRepositoryStub, addProductRepositoryStub };
 };
 
 describe('DbAddProduct Adapter', () => {
@@ -52,6 +77,27 @@ describe('DbAddProduct Adapter', () => {
     const { sut, findWalletByIdRepositoryStub } = makeSut();
     jest
       .spyOn(findWalletByIdRepositoryStub, 'findById')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const walletId = 'any_id';
+    const data = {
+      name: 'any_code',
+      quantity: 10,
+      price: 29.8,
+      purchase_date: new Date('2021-08-21'),
+      fees: 0.51,
+    };
+
+    const promise = sut.add(walletId, data);
+
+    await expect(promise).rejects.toThrow();
+  });
+
+  test('should calls DbAddProduct throws AddProductRepository throws', async () => {
+    const { sut, addProductRepositoryStub } = makeSut();
+    jest
+      .spyOn(addProductRepositoryStub, 'add')
       .mockReturnValueOnce(
         new Promise((resolve, reject) => reject(new Error())),
       );
