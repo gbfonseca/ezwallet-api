57fb5a7ebe7a97218f3c650265d131d879d36a46
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-10 13:32:11 -0300

    feat: ensure AddProductController return 400 if invalid purchase_date provided

--- src/presentation/controllers/product/add-product/add-product.ts
+++ src/presentation/controllers/product/add-product/add-product.ts
@@ -2,6 +2,7 @@ import { Controller } from '../../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../../protocols/http';
 import { badRequest } from '../../../helpers/http-helper';
 import { MissingParamError } from '../../../errors/missing-param-error';
+import { InvalidParamError } from '../../../errors/invalid-param-error';
 
 interface RequestBodyType {
   name: string;
@@ -20,11 +21,16 @@ export class AddProductController implements Controller {
     httpRequest: HttpRequest<RequestBodyType, RequestParamsType>,
   ): Promise<HttpResponse<any>> {
     const requiredFields = ['name', 'quantity', 'price', 'purchase_date'];
+    const { purchase_date } = httpRequest.body;
 
     for (const field in requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+
+    if (isNaN(new Date(purchase_date).getTime())) {
+      return badRequest(new InvalidParamError('purchase_date'));
     }
 
     return new Promise((resolve) => resolve(null));
