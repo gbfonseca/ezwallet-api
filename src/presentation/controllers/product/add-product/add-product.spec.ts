b20e4027784197780c5e3952b3ad02b52aecd704
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-10 14:04:42 -0300

    test: ensure AddProductController return 500 if AddProduct throws

--- src/presentation/controllers/product/add-product/add-product.spec.ts
+++ src/presentation/controllers/product/add-product/add-product.spec.ts
@@ -1,14 +1,45 @@
 import { HttpRequest } from '../../../protocols/http';
 import { AddProductController } from './add-product';
-
+import {
+  AddProduct,
+  AddProductModel,
+} from '../../../../domain/usecases/product/add-product';
+import { ProductModel } from '../../../../domain/models/product';
+import { fakeWallet } from '../../../../../tests/factories/fake-wallet';
 interface SutTypes {
   sut: AddProductController;
+  addProductStub: AddProduct;
 }
 
-const makeSut = (): SutTypes => {
-  const sut = new AddProductController();
+const makeAddProduct = (): AddProduct => {
+  class AddProductStub implements AddProduct {
+    async add(
+      walletId: string,
+      addProduct: AddProductModel,
+    ): Promise<ProductModel> {
+      const fakeProduct = {
+        id: 'any_id',
+        name: 'any_code',
+        quantity: 10,
+        price: 29.8,
+        purchase_date: new Date('2021-08-27'),
+        fees: 0.51,
+        wallet: {
+          id: 'any_id',
+          ...fakeWallet,
+        },
+      };
+      return new Promise((resolve) => resolve(fakeProduct));
+    }
+  }
 
-  return { sut };
+  return new AddProductStub();
+};
+const makeSut = (): SutTypes => {
+  const addProductStub = makeAddProduct();
+  const sut = new AddProductController(addProductStub);
+
+  return { sut, addProductStub };
 };
 
 describe('AddProduct Controller', () => {
@@ -130,5 +161,29 @@ describe('AddProduct Controller', () => {
     const httpResponse = await sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test('should return 500 if AddProduct throws', async () => {
+    const { sut, addProductStub } = makeSut();
+    jest
+      .spyOn(addProductStub, 'add')
+      .mockImplementationOnce(
+        () => new Promise((resolve, reject) => reject(new Error())),
+      );
+    const httpRequest: HttpRequest = {
+      params: {
+        walletId: 'any_id',
+      },
+      body: {
+        name: 'any_code',
+        quantity: 10,
+        price: 29.8,
+        purchase_date: '2021-08-21',
+        fees: 0.51,
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
