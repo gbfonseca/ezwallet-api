c8821a3f869b23bed6f43c88da8e64d5a8e234eb
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-10 12:57:32 -0300

    feat: ensure AddProductController return 400 if no name provided

--- src/presentation/controllers/product/add-product/add-product.ts
+++ src/presentation/controllers/product/add-product/add-product.ts
@@ -1,12 +1,14 @@
 import { Controller } from '../../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../../protocols/http';
+import { badRequest } from '../../../helpers/http-helper';
+import { MissingParamError } from '../../../errors/missing-param-error';
 
 interface RequestBodyType {
   name: string;
   quantity: number;
   price: number;
   purchase_date: Date;
-  fees: number;
+  fees?: number;
 }
 
 interface RequestParamsType {
@@ -17,6 +19,14 @@ export class AddProductController implements Controller {
   async handle(
     httpRequest: HttpRequest<RequestBodyType, RequestParamsType>,
   ): Promise<HttpResponse<any>> {
+    const requiredFields = ['name', 'quantity', 'price', 'purchase_date'];
+
+    for (const field in requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
+    }
+
     return new Promise((resolve) => resolve(null));
   }
 }
