4632797dbe0d745c55920e9df944b3a65df3cf40
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-10 16:30:08 -0300

    test: ensure DbAddProductAdapter throws if FindWalletByIdRepository throws

--- src/data/usecases/product/db-add-product-adapter/db-add-product-adapter.spec.ts
+++ src/data/usecases/product/db-add-product-adapter/db-add-product-adapter.spec.ts
@@ -1,13 +1,33 @@
+import { WalletModel } from '../../../../domain/models/wallet';
 import { DbAddProductAdapter } from './db-add-product-adapter';
+import { fakeWallet } from '../../../../../tests/factories/fake-wallet';
+import { FindWalletByIdRepository } from '../../../protocols/find-wallet-by-id-repository';
 
 interface SutTypes {
   sut: DbAddProductAdapter;
+  findWalletByIdRepositoryStub: FindWalletByIdRepository;
 }
 
-const makeSut = (): SutTypes => {
-  const sut = new DbAddProductAdapter();
+const makeFindWalletByIdRepository = (): FindWalletByIdRepository => {
+  class FindWalletByIdRepositoryStub implements FindWalletByIdRepository {
+    async findById(id: string): Promise<WalletModel> {
+      return new Promise((resolve) =>
+        resolve({
+          id,
+          ...fakeWallet,
+        }),
+      );
+    }
+  }
 
-  return { sut };
+  return new FindWalletByIdRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const findWalletByIdRepositoryStub = makeFindWalletByIdRepository();
+  const sut = new DbAddProductAdapter(findWalletByIdRepositoryStub);
+
+  return { sut, findWalletByIdRepositoryStub };
 };
 
 describe('DbAddProduct Adapter', () => {
@@ -26,5 +46,26 @@ describe('DbAddProduct Adapter', () => {
     await sut.add(walletId, data);
 
     expect(sutSpy).toHaveBeenCalledWith(walletId, data);
+  });
+
+  test('should calls DbAddProduct throws FindWalletByIdRepository throws', async () => {
+    const { sut, findWalletByIdRepositoryStub } = makeSut();
+    jest
+      .spyOn(findWalletByIdRepositoryStub, 'findById')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const walletId = 'any_id';
+    const data = {
+      name: 'any_code',
+      quantity: 10,
+      price: 29.8,
+      purchase_date: new Date('2021-08-21'),
+      fees: 0.51,
+    };
+
+    const promise = sut.add(walletId, data);
+
+    await expect(promise).rejects.toThrow();
   });
 });
