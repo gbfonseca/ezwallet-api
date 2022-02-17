642a10daaf6da58a401003535f6c4b3641a5f4fa
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-02-17 20:12:35 -0300

    feat: ensure SetPrimaryWalletContrller return 400 if no walletId provided

--- src/presentation/controllers/wallet/set-primary-wallet/set-primary-wallet.ts
+++ src/presentation/controllers/wallet/set-primary-wallet/set-primary-wallet.ts
@@ -1,13 +1,23 @@
 import { Controller } from '../../../protocols/controller';
 import {
+  badRequest,
   HttpRequest,
   HttpResponse,
+  MissingParamError,
 } from '../create-wallet/create-wallet-protocols';
 
 export class SetPrimaryWalletController implements Controller {
   async handle(
-    httpRequest: HttpRequest<void, { walletId: string }>,
+    httpRequest: HttpRequest<unknown, { walletId: string }>,
   ): Promise<HttpResponse<any>> {
+    const {
+      params: { walletId },
+    } = httpRequest;
+
+    if (!walletId) {
+      return badRequest(new MissingParamError('walletId'));
+    }
+
     return new Promise((resolve) => resolve(null));
   }
 }
