a1a2c0b936d2f99df777a2f6a7372e1ca03ef7a0
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-02-17 17:55:11 -0300

    test: ensure SetPrimaryWalletContrller called with the correct values

---
+++ src/presentation/controllers/wallet/set-primary-wallet/set-primary-wallet.spec.ts
@@ -0,0 +1,30 @@
+import { HttpRequest } from '../../../protocols/http';
+import { SetPrimaryWalletController } from './set-primary-wallet';
+
+interface SutTypes {
+  sut: SetPrimaryWalletController;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new SetPrimaryWalletController();
+
+  return { sut };
+};
+
+describe('SetPrimaryWallet Controller', () => {
+  it('should calls handle with correct values', async () => {
+    const { sut } = makeSut();
+
+    const sutSpy = jest.spyOn(sut, 'handle');
+
+    const httpRequest: HttpRequest = {
+      params: {
+        walletId: 'any_id',
+      },
+    };
+
+    await sut.handle(httpRequest);
+
+    expect(sutSpy).toHaveBeenCalledWith(httpRequest);
+  });
+});
