68ed95d737f921a6859a040c15ebec2937a3dd8a
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-02-18 12:11:59 -0300

    test: ensure DbGetPrimaryWalletAdapter throws if GetPrimaryWalletRepository throws

--- src/data/usecases/wallet/db-get-primary-wallet-adapter/db-get-primary-wallet-adapter.spec.ts
+++ src/data/usecases/wallet/db-get-primary-wallet-adapter/db-get-primary-wallet-adapter.spec.ts
@@ -1,13 +1,27 @@
+import { WalletModel } from '../../../../domain/models/wallet';
+import { GetPrimaryWalletRepository } from '../../../protocols/get-primary-wallet-repository';
 import { DbGetPrimaryWalletAdapter } from './db-get-primary-wallet-adapter';
-
+import { fakeWalletCreated } from '../../../../../tests/factories/fake-wallet';
 interface SutTypes {
   sut: DbGetPrimaryWalletAdapter;
+  getPrimaryWalletRepositoryStub: GetPrimaryWalletRepository;
 }
 
-const makeSut = (): SutTypes => {
-  const sut = new DbGetPrimaryWalletAdapter();
+const makeGetPrimaryWalletRepository = (): GetPrimaryWalletRepository => {
+  class GetPrimaryWalletRepositoryStub implements GetPrimaryWalletRepository {
+    async getPrimary(userId: string): Promise<WalletModel> {
+      return new Promise((resolve) => resolve(fakeWalletCreated));
+    }
+  }
 
-  return { sut };
+  return new GetPrimaryWalletRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const getPrimaryWalletRepositoryStub = makeGetPrimaryWalletRepository();
+  const sut = new DbGetPrimaryWalletAdapter(getPrimaryWalletRepositoryStub);
+
+  return { sut, getPrimaryWalletRepositoryStub };
 };
 
 describe('DbGetPrimaryWallet Adapter', () => {
@@ -21,5 +35,21 @@ describe('DbGetPrimaryWallet Adapter', () => {
     await sut.getPrimary(user_id);
 
     expect(sutSpy).toHaveBeenCalledWith(user_id);
+  });
+
+  it('should throws if GetPrimaryWalletRepository throws', async () => {
+    const { sut, getPrimaryWalletRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(getPrimaryWalletRepositoryStub, 'getPrimary')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+
+    const user_id = 'any_id';
+
+    const promise = sut.getPrimary(user_id);
+
+    await expect(promise).rejects.toThrow();
   });
 });
