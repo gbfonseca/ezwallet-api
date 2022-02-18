338132e62c421160823a500a3b42e3edeba1eb3c
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-02-18 11:18:11 -0300

    feat: ensure GetPrimaryWalletController return 400 if no user provided

--- src/presentation/controllers/wallet/get-primary-wallet/get-primary-wallet.ts
+++ src/presentation/controllers/wallet/get-primary-wallet/get-primary-wallet.ts
@@ -1,8 +1,15 @@
 import { Controller } from '../../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../../protocols/http';
+import { badRequest } from '../create-wallet/create-wallet-protocols';
 
 export class GetPrimaryWalletController implements Controller {
   async handle(httpRequest: HttpRequest<any, any>): Promise<HttpResponse<any>> {
+    const { user } = httpRequest;
+
+    if (!user) {
+      return badRequest(new Error('Usuário Inválido'));
+    }
+
     return new Promise((resolve) => resolve(null));
   }
 }
