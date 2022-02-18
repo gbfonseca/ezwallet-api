cc8f0473f58d3c9646dd45cabba2fcc1b817ca3d
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-02-17 21:18:50 -0300

    test: ensure DbSetPrimaryWalletAdapter throws if SetPrimaryWalletRepository throws

--- src/data/usecases/wallet/db-set-primary-wallet-adapter/db-set-primary-wallet.spec.ts
+++ src/data/usecases/wallet/db-set-primary-wallet-adapter/db-set-primary-wallet.spec.ts
@@ -1,14 +1,30 @@
 import { DbSetPrimaryWalletAdapter } from './db-set-primary-wallet';
-
+import { SetPrimaryWalletRepository } from '../../../protocols/set-primary-wallet-repository';
+import { WalletModel } from '../../../../domain/models/wallet';
+import { fakeWalletCreated } from '../../../../../tests/factories/fake-wallet';
 interface SutTypes {
   sut: DbSetPrimaryWalletAdapter;
+  setPrimaryWalletRepositoryStub: SetPrimaryWalletRepository;
 }
 
-const makeSut = (): SutTypes => {
-  const sut = new DbSetPrimaryWalletAdapter();
+const makeSetPrimaryWalletRepository = (): SetPrimaryWalletRepository => {
+  class SetPrimaryWalletRepositoryStub implements SetPrimaryWalletRepository {
+    setPrimary(walletId: string): Promise<WalletModel> {
+      return new Promise((resolve) => resolve(fakeWalletCreated));
+    }
+  }
 
-  return { sut };
+  return new SetPrimaryWalletRepositoryStub();
 };
+
+const makeSut = (): SutTypes => {
+  const setPrimaryWalletRepositoryStub = makeSetPrimaryWalletRepository();
+
+  const sut = new DbSetPrimaryWalletAdapter(setPrimaryWalletRepositoryStub);
+
+  return { sut, setPrimaryWalletRepositoryStub };
+};
+
 describe('DbSetPrimaryWalletAdapter', () => {
   it('should calls DbSetPrimaryWallet with correct values', async () => {
     const { sut } = makeSut();
@@ -20,5 +36,21 @@ describe('DbSetPrimaryWalletAdapter', () => {
     await sut.setPrimary(walletId);
 
     expect(sutSpy).toHaveBeenCalledWith(walletId);
+  });
+
+  it('should throws if SetPrimaryWalletRepository throws', async () => {
+    const { sut, setPrimaryWalletRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(setPrimaryWalletRepositoryStub, 'setPrimary')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+
+    const walletId = 'any_id';
+
+    const promise = sut.setPrimary(walletId);
+
+    await expect(promise).rejects.toThrow();
   });
 });
