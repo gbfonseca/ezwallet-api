e594529d4ce9d86cda8663b5f1e9153cd886271a
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-02-17 22:40:06 -0300

    test: compose AddProduct route

---
+++ src/main/routes/add-product-routes.test.ts
@@ -0,0 +1,57 @@
+import { TypeormHelper } from './../../infra/db/typeorm/helpers/typeorm-helper';
+import request from 'supertest';
+import app from '../config/app';
+import setupMiddlewares from '../config/middlewares';
+import setupRoutes from '../config/routes';
+import { fakeUser } from '../../../tests/factories/fake-user';
+import { fakeProduct } from '../../../tests/factories/fake-product';
+
+import { fakeWallet } from '../../../tests/factories/fake-wallet';
+describe('AddWallet route', () => {
+  beforeAll(async () => {
+    await TypeormHelper.connect({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: ['src/infra/db/typeorm/entities/**.ts'],
+      migrations: ['src/infra/db/typeorm/migrations/**.ts'],
+      synchronize: true,
+      cli: {
+        migrationsDir: './src/infra/db/typeorm/migrations',
+        entitiesDir: './src/infra/db/typeorm/entities',
+      },
+    });
+    setupMiddlewares(app);
+    setupRoutes(app);
+  });
+
+  afterAll(async () => {
+    await TypeormHelper.client.close();
+  });
+
+  beforeEach(async () => {
+    await TypeormHelper.clear();
+  });
+  test('should return an user', async () => {
+    await request(app).post('/api/auth/signup').send(fakeUser).expect(200);
+
+    const { body } = await request(app)
+      .post('/api/auth/signin')
+      .send({
+        email: fakeUser.email,
+        password: fakeUser.password,
+      })
+      .expect(200);
+
+    const { body: wallet } = await request(app)
+      .post('/api/wallet/create')
+      .set('Authorization', `Bearer ${body.token}`)
+      .send(fakeWallet)
+      .expect(200);
+
+    await request(app)
+      .post(`/api/product/${wallet.id}/add`)
+      .set('Authorization', `Bearer ${body.token}`)
+      .send(fakeProduct)
+      .expect(200);
+  });
+});
